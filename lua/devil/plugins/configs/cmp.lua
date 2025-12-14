3a37ca2239cb93399728e58b675573b2802c8647
MysticalDevil <jinpeng7981@outlook.com>
2025-12-15 00:51:53 +0800

    feat(blink.cmp): enable and customize some blink.cmp's features

--- lua/devil/plugins/configs/cmp.lua
+++ lua/devil/plugins/configs/cmp.lua
@@ -34,13 +34,35 @@ return {
   -- elsewhere in your config, without redefining it, due to `opts_extend`
   sources = {
     default = { "lsp", "path", "snippets", "buffer" },
+    providers = {
+      lsp = {
+        min_keyword_length = 2, -- Number of characters to trigger porvider
+        score_offset = 0,       -- Boost/penalize the score of the items
+      },
+      path = {
+        min_keyword_length = 0,
+      },
+      snippets = {
+        min_keyword_length = 2,
+      },
+      buffer = {
+        min_keyword_length = 5,
+        max_items = 5,
+      },
+    },
   },
   completion = {
     ghost_text = { enabled = true },
+    documentation = {
+      auto_show = true,
+      auto_show_delay_ms = 250,
+      treesitter_highlighting = true,
+      window = { border = "single" },
+    },
     menu = {
+      border = "single",
       -- Don't automatically show the completion menu
       auto_show = true,
-
       -- Delay before showing the completion menu while typing
       auto_show_delay_ms = 500,
 
@@ -50,9 +72,56 @@ return {
           { "label",     "label_description", gap = 1 },
           { "kind_icon", "kind",              gap = 1 },
         },
+        components = {
+          kind_icon = {
+            text = function(ctx)
+              local icon = ctx.kind_icon
+              if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+                if dev_icon then
+                  icon = dev_icon
+                end
+              else
+                icon = require("lspkind").symbolic(ctx.kind, {
+                  mode = "symbol",
+                })
+              end
+
+              return icon .. ctx.icon_gap
+            end,
+
+            -- Optionally, use the highlight groups from nvim-web-devicons
+            -- You can also add the same function for `kind.highlight` if you want to
+            -- keep the highlight groups in sync with the icons.
+            highlight = function(ctx)
+              local hl = ctx.kind_hl
+              if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+                if dev_icon then
+                  hl = dev_hl
+                end
+              end
+              return hl
+            end,
+          }
+        }
       },
 
+      -- Avoid multi-line completion ghost text
       direction_priority = cmp_ghost_text
     },
   },
+
+  fuzzy = {
+    sorts = {
+      function(a, b)
+        if (a.client_name == nil or b.client_name == nil) or (a.client_name == b.client_name) then
+          return
+        end
+        return b.client_name == "emmet_ls"
+      end,
+      "score",
+      "sort_text",
+    }
+  }
 }
--- lua/devil/plugins/configs/lspconfig.lua
+++ lua/devil/plugins/configs/lspconfig.lua
@@ -74,7 +74,6 @@ local runtime_path = vim.split(package.path, ";", {})
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/>/init.lua")
 
-require("neodev").setup()
 local lua_ls = {
   settings = {
     Lua = {
