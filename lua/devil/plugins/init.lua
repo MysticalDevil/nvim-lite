62e49966d387f9c0e5b1f6eef47f7da617107a2e
MysticalDevil <jinpeng7981@outlook.com>
2026-01-27 23:09:27 +0800

    fix: fix smart-open.nvim deps

--- lua/devil/plugins/init.lua
+++ lua/devil/plugins/init.lua
@@ -345,10 +345,7 @@ local plugins_list = {
       branch = "0.2.x",
       dependencies = {
         "kkharji/sqlite.lua",
-        -- Only required if using match_algorithm fzf
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-        -- Optional.  If installed, native fzy will be used when match_algorithm is fzy
-        { "nvim-telescope/telescope-fzy-native.nvim" },
       },
     },
 
