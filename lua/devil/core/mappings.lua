22e970f7b7d64422c4367001b132fabd01ce96d6
MysticalDevil <jinpeng7981@outlook.com>
2026-02-16 21:39:41 +0800

    fix(lsp): toggle inlay hints per-buffer

--- lua/devil/core/mappings.lua
+++ lua/devil/core/mappings.lua
@@ -220,7 +220,9 @@ M.lspconfig = {
 
     ["<leader>L"] = {
       function()
-        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ 0 }), { 0 })
+        local bufnr = vim.api.nvim_get_current_buf()
+        local enabled = vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr })
+        vim.lsp.inlay_hint.enable(not enabled, { bufnr = bufnr })
       end,
       "Toggle LSP inlay hints",
     },
