26bb85b5adb04c2474909b672f29972f51645641
MysticalDevil <jinpeng7981@outlook.com>
2026-04-23 23:50:44 +0800

    fix(heirline): guard ScrollBar against division by zero

--- lua/devil/plugins/configs/heirline.lua
+++ lua/devil/plugins/configs/heirline.lua
@@ -239,7 +239,7 @@ local ScrollBar = {
   provider = function(self)
     local curr_line = vim.api.nvim_win_get_cursor(0)[1]
     local lines = vim.api.nvim_buf_line_count(0)
-    local i = math.floor((curr_line - 1) / lines * #self.sbar) + 1
+    local i = lines > 0 and (math.floor((curr_line - 1) / lines * #self.sbar) + 1) or 1
     return string.rep(self.sbar[i], 2)
   end,
   hl = { fg = "blue", bg = "bright_bg" },
