7ac110ee6599c61b32fac0c137963c649b6d14f6
MysticalDevil <jinpeng7981@outlook.com>
2026-05-24 18:08:33 +0800

    feat: add quicker.nvim for improved quickfix UI and editing

--- lua/devil/plugins/specs/core.lua
+++ lua/devil/plugins/specs/core.lua
@@ -225,6 +225,45 @@ return function()
     },
 
     {
+      "stevearc/quicker.nvim",
+      ft = "qf",
+      opts = {
+        keys = {
+          {
+            ">",
+            function()
+              require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+            end,
+            desc = "Expand quickfix context",
+          },
+          {
+            "<",
+            function()
+              require("quicker").collapse()
+            end,
+            desc = "Collapse quickfix context",
+          },
+        },
+      },
+      keys = {
+        {
+          "<leader>qq",
+          function()
+            require("quicker").toggle()
+          end,
+          desc = "Toggle quickfix",
+        },
+        {
+          "<leader>ql",
+          function()
+            require("quicker").toggle({ loclist = true })
+          end,
+          desc = "Toggle loclist",
+        },
+      },
+    },
+
+    {
       "folke/trouble.nvim",
       event = "LspAttach",
       cmd = "Trouble",
